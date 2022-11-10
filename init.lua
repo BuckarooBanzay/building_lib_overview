ae98de673cf0b2d7675a5c2a365998a5493a40e2
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-11-10 19:52:20 +0100

    init

---
+++ init.lua
@@ -0,0 +1,9 @@
+local MP = minetest.get_modpath("building_lib_overview")
+
+building_lib_overview = {}
+
+dofile(MP .. "/overview.lua")
+
+if minetest.get_modpath("mtt") and mtt.enabled then
+	dofile(MP .. "/overview.spec.lua")
+end
