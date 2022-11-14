bd219cf0741af48756630a931181a6f966b66efa
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-11-14 09:01:07 +0100

    remove buildings on overview

--- overview.lua
+++ overview.lua
@@ -31,3 +31,14 @@ end)
 building_lib.register_on("placed", function(mapblock_pos, _, building_def, rotation, size)
     place_overview_node(mapblock_pos, building_def.overview, rotation, size)
 end)
+
+building_lib.register_on("removed", function(mapblock_pos, _, building_info)
+    local size = building_info.size or {x=1, y=1, z=1}
+    local mapblock_pos2 = vector.add(mapblock_pos, vector.subtract(size, 1))
+
+    mapblock_lib.for_each(mapblock_pos, mapblock_pos2, function(offset_mapblock_pos)
+        local node_pos = building_lib_overview.mapblock_pos_to_overview(offset_mapblock_pos)
+        minetest.load_area(node_pos)
+        minetest.set_node(node_pos, { name = "air" })
+    end)
+end)
